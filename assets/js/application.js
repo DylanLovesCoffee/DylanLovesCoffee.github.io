e4446707c4947f3730d7a82911097f7da16b811a
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-08-29 14:12:25 -0400

    Add animation after form is submitted

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $("#email-form").on("submit", function(e){
     e.preventDefault();
-    console.log("HELP");
+    $(".contact-form").empty();
+    $(".contact-form").append(
+      "<div class='thanks container' style='display:none'>
+        <h4>Thanks for reaching out!</h4>
+        <p>I'll get back to you as soon as possible.</p>
+      </div>"
+    );
+    $(".thanks").fadeIn(2000);
   });
 });
