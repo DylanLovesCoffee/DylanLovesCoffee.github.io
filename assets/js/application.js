05040d0bc07a2ea714199e4ddbe500353f9ff41b
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-08-30 22:39:36 -0400

    Send post request to heroku server. Email forward to inbox

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   $("#email-form").on("submit", function(e){
     e.preventDefault();
+    var $form = $(this)
+    $.ajax({
+      url: $form.attr('action'),
+      method: $form.attr('method'),
+      data: $form.serialize()
+    }).done(function(data){
+      console.log(data)
+    });
     $(".contact-form").empty();
     $(".contact-form").append("<div class='thanks container' style='display:none'><h4>Thanks for reaching out!</h4><p>I'll get back to you as soon as possible.</p></div>");
     $(".thanks").fadeIn(2000);
