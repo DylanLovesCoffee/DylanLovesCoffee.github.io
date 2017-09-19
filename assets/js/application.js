4407271b6eb3995c62107d3629e7bc41f5520928
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-09-18 21:08:55 -0400

    Open Github page in new tab when View Project is clicked.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-  $("#email-form").on("submit", function(e){
+  $("#email-form").on("submit", function(e) {
     e.preventDefault();
     var $form = $(this)
     $.ajax({
       url: $form.attr('action'),
       method: $form.attr('method'),
       data: $form.serialize()
-    }).done(function(data){
+    }).done(function(data) {
       console.log(data)
     });
     $(".contact-form").empty();
     $(".contact-form").append("<div class='thanks container' style='display:none'><h4>Thanks for reaching out!</h4><p>I'll get back to you as soon as possible.</p></div>");
     $(".thanks").fadeIn(2000);
   });
+
+  $("#forage-button").on("click", function() {
+    window.open("https://github.com/DylanLovesCoffee/forage")
+  });
+
+  $("#omakase-button").on("click", function() {
+    window.open("https://github.com/DylanLovesCoffee/omakase-frontend")
+  });
+
 });
